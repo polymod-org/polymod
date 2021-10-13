6babd6d9b16f6b216b8f3fda6935aec0ab1ac34c
Oj <oj@oojmed.com>
2021-10-13 08:44:46 +0100

    [Content] Use VSCode standard again

--- content.js
+++ content.js
@@ -72,50 +72,20 @@ const loadPlugin = async ({ file, host, source }) => {
 
   if (loaded[file].vscode) { // VSCode Theme embedded
     const theme = loaded[file].vscode;
+    const VSCode = await import(`https://standard.polymod.dev/theme-compat/vscode.js`);
 
-    let el;
-
-    theme.tokenColors = theme.tokenColors.map((x) => { // Standardise token color scopes (string -> array)
-      if (typeof x.scope === 'string') {
-        x.scope = x.scope.split(',').map((y) => y.trim());
-      }
-  
-      return x;
-    });
-  
-    const findTokenColor = (scopeSegment) => theme.tokenColors.find((x) => x.scope.includes(scopeSegment));
-
-    const css = `.theme-dark, .theme-light {
-      --background-primary: ${theme.colors['editor.background']}; /* Main editor background */
-      --background-secondary: ${theme.colors['sideBar.background']}; /* Sidebar (left or right with file explorer, etc.) */
-      --background-secondary-alt: ${theme.colors['input.background']};
-      --background-tertiary: ${theme.colors['activityBar.background']};
-    
-      --channeltextarea-background: var(--background-secondary-alt);
-    
-      --background-accent: ${theme.colors['editorIndentGuide.background']}; /* Background for inputs */
-      --background-floating: ${theme.colors['button.secondaryBackground']}; /* Background for buttons */
-    
-      --brand-experiment: ${theme.colors['textLink.foreground']}; /* Background for links */
-    
-      --text-link: var(--brand-experiment);
-      --text-normal: ${theme.colors['editor.foreground']}; /* Foreground for main editor */
-      --text-muted: ${findTokenColor('comment').settings.foreground};
-      --interactive-normal: ${theme.colors['descriptionForeground']}; /* Foreground for descriptions */
-      --interactive-hover: var(--text-normal);
-      --interactive-active: ${theme.colors['settings.headerForeground']}; /* Foreground for headers */
-      --interactive-muted: var(--text-muted);
-    }`;
+    let css;
 
     loaded[file] = {
       load: async () => {
-        el = pureCSS(css);
+        css = VSCode.add(theme);
 
         loaded[file].cssCache.add(css);
       },
+  
 
       unload: () => {
-        if (el) el.remove();
+        VSCode.remove();
 
         loaded[file].cssCache.remove(css);
       }
